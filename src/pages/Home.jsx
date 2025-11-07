03aa33876924676de374fd8f330044ada3f89794
digitalqurani <mdigitalqurani@gmail.com>
2025-11-07 12:30:01 +0700

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Hero from "../components/Hero";
+import Programs from "../components/Programs";
+import DaftarForm from "../components/DaftarForm";
+import Kontak from "../components/Kontak";
+export default function Home(){ return (<><Hero /><Programs /><DaftarForm /><Kontak /></>);}
