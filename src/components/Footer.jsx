f6e0737fa747ada307abdf6f8278a661ea7fe861
digitalqurani <mdigitalqurani@gmail.com>
2025-11-07 12:28:18 +0700

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,2 @@
+import React from "react";
+export default function Footer(){ return (<footer className="bg-teal-700 text-white py-6 text-center"><p>&copy; {new Date().getFullYear()} Madrasah Diniyah IT & Qur'an. All rights reserved.</p></footer>);}
